360e00ab8e34a882d87ee101f723a9aa16647b90
vanarielb <van.ariel.bugayong@gmail.com>
2024-08-16 09:22:54 +0800

    fix port number

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ app.get('/', (req, res) => {
 
 app.use(ProfileRoute);
 
-const PORT = 3333;
+// const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
